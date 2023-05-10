27577583c85e5286155db8b447541674829752d5
TexnoPark <TexnoPark@102>
2023-05-10 19:40:19 +0500

    Добавьте файлы проекта.

---
+++ XO/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace XO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.x1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.x2 = new System.Windows.Forms.Button();
+            this.x3 = new System.Windows.Forms.Button();
+            this.x4 = new System.Windows.Forms.Button();
+            this.x5 = new System.Windows.Forms.Button();
+            this.x6 = new System.Windows.Forms.Button();
+            this.x7 = new System.Windows.Forms.Button();
+            this.x8 = new System.Windows.Forms.Button();
+            this.x9 = new System.Windows.Forms.Button();
+            this.lbl_balX = new System.Windows.Forms.Label();
+            this.lbl_balY = new System.Windows.Forms.Label();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.guna2GradientButton1 = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.guna2GradientButton2 = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.guna2GradientButton3 = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // x1
+            // 
+            this.x1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.x1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.x1.Location = new System.Drawing.Point(3, 3);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(100, 100);
+            this.x1.TabIndex = 0;
+            this.x1.UseVisualStyleBackColor = false;
+            this.x1.Click += new System.EventHandler(this.x1_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel1.Controls.Add(this.x1);
+            this.flowLayoutPanel1.Controls.Add(this.x2);
+            this.flowLayoutPanel1.Controls.Add(this.x3);
+            this.flowLayoutPanel1.Controls.Add(this.x4);
+            this.flowLayoutPanel1.Controls.Add(this.x5);
+            this.flowLayoutPanel1.Controls.Add(this.x6);
+            this.flowLayoutPanel1.Controls.Add(this.x7);
+            this.flowLayoutPanel1.Controls.Add(this.x8);
+            this.flowLayoutPanel1.Controls.Add(this.x9);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(320, 319);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // x2
+            // 
+            this.x2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.x2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.x2.Location = new System.Drawing.Point(109, 3);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(100, 100);
+            this.x2.TabIndex = 1;
+            this.x2.UseVisualStyleBackColor = false;
+            this.x2.Click += new System.EventHandler(this.x1_Click);
+            // 
+            // x3
+            // 
+            this.x3.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.x3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.x3.Location = new System.Drawing.Point(215, 3);
+            this.x3.Name = "x3";
+            this.x3.Size = new System.Drawing.Size(100, 100);
+            this.x3.TabIndex = 2;
+            this.x3.UseVisualStyleBackColor = false;
+            this.x3.Click += new System.EventHandler(this.x1_Click);
+            // 
+            // x4
+            // 
+            this.x4.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.x4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.x4.Location = new System.Drawing.Point(3, 109);
+            this.x4.Name = "x4";
+            this.x4.Size = new System.Drawing.Size(100, 100);
+            this.x4.TabIndex = 3;
+            this.x4.UseVisualStyleBackColor = false;
+            this.x4.Click += new System.EventHandler(this.x1_Click);
+            // 
+            // x5
+            // 
+            this.x5.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.x5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.x5.Location = new System.Drawing.Point(109, 109);
+            this.x5.Name = "x5";
+            this.x5.Size = new System.Drawing.Size(100, 100);
+            this.x5.TabIndex = 4;
+            this.x5.UseVisualStyleBackColor = false;
+            this.x5.Click += new System.EventHandler(this.x1_Click);
+            // 
+            // x6
+            // 
+            this.x6.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.x6.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.x6.Location = new System.Drawing.Point(215, 109);
+            this.x6.Name = "x6";
+            this.x6.Size = new System.Drawing.Size(100, 100);
+            this.x6.TabIndex = 5;
+            this.x6.UseVisualStyleBackColor = false;
+            this.x6.Click += new System.EventHandler(this.x1_Click);
+            // 
+            // x7
+            // 
+            this.x7.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.x7.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.x7.Location = new System.Drawing.Point(3, 215);
+            this.x7.Name = "x7";
+            this.x7.Size = new System.Drawing.Size(100, 100);
+            this.x7.TabIndex = 6;
+            this.x7.UseVisualStyleBackColor = false;
+            this.x7.Click += new System.EventHandler(this.x1_Click);
+            // 
+            // x8
+            // 
+            this.x8.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.x8.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.x8.Location = new System.Drawing.Point(109, 215);
+            this.x8.Name = "x8";
+            this.x8.Size = new System.Drawing.Size(100, 100);
+            this.x8.TabIndex = 7;
+            this.x8.UseVisualStyleBackColor = false;
+            this.x8.Click += new System.EventHandler(this.x1_Click);
+            // 
+            // x9
+            // 
+            this.x9.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.x9.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.x9.Location = new System.Drawing.Point(215, 215);
+            this.x9.Name = "x9";
+            this.x9.Size = new System.Drawing.Size(100, 100);
+            this.x9.TabIndex = 8;
+            this.x9.UseVisualStyleBackColor = false;
+            this.x9.Click += new System.EventHandler(this.x1_Click);
+            // 
+            // lbl_balX
+            // 
+            this.lbl_balX.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_balX.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_balX.ForeColor = System.Drawing.Color.Red;
+            this.lbl_balX.Location = new System.Drawing.Point(496, 59);
+            this.lbl_balX.Name = "lbl_balX";
+            this.lbl_balX.Size = new System.Drawing.Size(121, 35);
+            this.lbl_balX.TabIndex = 2;
+            this.lbl_balX.Text = "lbl_balX: 0";
+            // 
+            // lbl_balY
+            // 
+            this.lbl_balY.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_balY.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_balY.ForeColor = System.Drawing.Color.Red;
+            this.lbl_balY.Location = new System.Drawing.Point(496, 121);
+            this.lbl_balY.Name = "lbl_balY";
+            this.lbl_balY.Size = new System.Drawing.Size(121, 30);
+            this.lbl_balY.TabIndex = 3;
+            this.lbl_balY.Text = "lbl_balX: 0";
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // guna2GradientButton1
+            // 
+            this.guna2GradientButton1.Animated = true;
+            this.guna2GradientButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2GradientButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2GradientButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2GradientButton1.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2GradientButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2GradientButton1.FillColor = System.Drawing.Color.DimGray;
+            this.guna2GradientButton1.FillColor2 = System.Drawing.Color.Red;
+            this.guna2GradientButton1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.guna2GradientButton1.ForeColor = System.Drawing.Color.White;
+            this.guna2GradientButton1.Location = new System.Drawing.Point(766, 0);
+            this.guna2GradientButton1.Name = "guna2GradientButton1";
+            this.guna2GradientButton1.Size = new System.Drawing.Size(34, 33);
+            this.guna2GradientButton1.TabIndex = 4;
+            this.guna2GradientButton1.Text = "X";
+            this.guna2GradientButton1.Click += new System.EventHandler(this.guna2GradientButton1_Click);
+            // 
+            // guna2GradientButton2
+            // 
+            this.guna2GradientButton2.Animated = true;
+            this.guna2GradientButton2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2GradientButton2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2GradientButton2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2GradientButton2.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2GradientButton2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2GradientButton2.FillColor = System.Drawing.Color.Lime;
+            this.guna2GradientButton2.FillColor2 = System.Drawing.Color.DimGray;
+            this.guna2GradientButton2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.guna2GradientButton2.ForeColor = System.Drawing.Color.White;
+            this.guna2GradientButton2.Location = new System.Drawing.Point(732, 0);
+            this.guna2GradientButton2.Name = "guna2GradientButton2";
+            this.guna2GradientButton2.Size = new System.Drawing.Size(34, 33);
+            this.guna2GradientButton2.TabIndex = 5;
+            this.guna2GradientButton2.Text = "_";
+            this.guna2GradientButton2.Click += new System.EventHandler(this.guna2GradientButton2_Click);
+            // 
+            // guna2GradientButton3
+            // 
+            this.guna2GradientButton3.Animated = true;
+            this.guna2GradientButton3.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2GradientButton3.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2GradientButton3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2GradientButton3.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2GradientButton3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2GradientButton3.FillColor = System.Drawing.Color.Gold;
+            this.guna2GradientButton3.FillColor2 = System.Drawing.Color.Lime;
+            this.guna2GradientButton3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.guna2GradientButton3.ForeColor = System.Drawing.Color.White;
+            this.guna2GradientButton3.Location = new System.Drawing.Point(699, 0);
+            this.guna2GradientButton3.Name = "guna2GradientButton3";
+            this.guna2GradientButton3.Size = new System.Drawing.Size(34, 33);
+            this.guna2GradientButton3.TabIndex = 6;
+            this.guna2GradientButton3.Text = "↻";
+            this.guna2GradientButton3.Click += new System.EventHandler(this.guna2GradientButton3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::XO.Properties.Resources.defaem;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.guna2GradientButton3);
+            this.Controls.Add(this.guna2GradientButton2);
+            this.Controls.Add(this.guna2GradientButton1);
+            this.Controls.Add(this.lbl_balY);
+            this.Controls.Add(this.lbl_balX);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button x1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button x2;
+        private System.Windows.Forms.Button x3;
+        private System.Windows.Forms.Button x4;
+        private System.Windows.Forms.Button x5;
+        private System.Windows.Forms.Button x6;
+        private System.Windows.Forms.Button x7;
+        private System.Windows.Forms.Button x8;
+        private System.Windows.Forms.Button x9;
+        private System.Windows.Forms.Label lbl_balX;
+        private System.Windows.Forms.Label lbl_balY;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2GradientButton guna2GradientButton2;
+        private Guna.UI2.WinForms.Guna2GradientButton guna2GradientButton1;
+        private Guna.UI2.WinForms.Guna2GradientButton guna2GradientButton3;
+    }
+}
+
---
+++ XO/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int sana = 0;
+
+
+
+        int ballX = 0, ballY = 0;
+
+        private void x1_Click(object sender, EventArgs e)
+        {
+           Button ajaga = (Button)sender;
+            X_OR_O(ajaga);
+            
+        }
+
+        private void guna2GradientButton1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void guna2GradientButton2_Click(object sender, EventArgs e)
+        {
+            this.SendToBack();
+        }
+
+        private void guna2GradientButton3_Click(object sender, EventArgs e)
+        {
+            lbl_balX.Text = "Ball X:0" ;
+            lbl_balY.Text = "Ball O:0" ;
+            x1.Text = "";
+            x2.Text = "";
+            x3.Text = "";
+            x4.Text = "";
+            x5.Text = "";
+            x6.Text = "";
+            x7.Text = "";
+            x8.Text = "";
+            x9.Text = "";
+            sana = 0;
+            ballX = 0; ballY = 0;
+            this.BackgroundImage = Properties.Resources.defaem;
+        }
+
+        void X_OR_O(Button btn)
+
+        {
+            if (btn.Text == "" && sana % 2 == 0)
+            {
+                btn.Text = "X";
+                sana++;
+
+            }
+            else if (btn.Text == "" && sana % 2 == 1)
+            {
+                btn.Text = "O";
+                sana++;
+
+            }
+            tekser("X");
+            tekser("O");
+            void tekser(string belgi)
+            {
+                if (x1.Text == belgi && x2.Text == belgi && x3.Text == belgi ||
+                   x4.Text == belgi && x5.Text == belgi && x6.Text == belgi ||
+                           x7.Text == belgi && x8.Text == belgi && x9.Text == belgi ||
+                           x1.Text == belgi && x4.Text == belgi && x7.Text == belgi ||
+                           x2.Text == belgi && x5.Text == belgi && x8.Text == belgi ||
+                           x3.Text == belgi && x6.Text == belgi && x9.Text == belgi ||
+                           x1.Text == belgi && x5.Text == belgi && x9.Text == belgi ||
+               x3.Text == belgi && x5.Text == belgi && x7.Text == belgi)
+                {
+
+                   
+                    if (belgi == "X")
+                    {
+                        this.BackgroundImage = Properties.Resources.X;
+                        MessageBox.Show(belgi + " Won the Game");
+                        this.BackgroundImage = Properties.Resources.defaem;
+                    }
+                    else if(belgi=="O")
+                    {
+                        this.BackgroundImage = Properties.Resources.O;
+                        MessageBox.Show(belgi + " Won the Game");
+                        this.BackgroundImage = Properties.Resources.defaem;
+                    }
+                   
+                    if (belgi == "X")
+                    {
+                        ballX++;
+                    }
+                    else if (belgi == "O")
+                    {
+                        ballY++;
+                    }
+                    Console.Beep();
+                    lbl_balX.Text = "Ball X: " + ballX;
+                    lbl_balY.Text = "Ball O: " + ballY;
+                    x1.Text = "";
+                    x2.Text = "";
+                    x3.Text = "";
+                    x4.Text = "";
+                    x5.Text = "";
+                    x6.Text = "";
+                    x7.Text = "";
+                    x8.Text = "";
+                    x9.Text = "";
+                    sana = 0;
+                }
+                else if(sana==9)
+                {
+                    this.BackgroundImage = Properties.Resources.XO;
+                    MessageBox.Show("Draw!");
+                    x1.Text = "";
+                    x2.Text = "";
+                    x3.Text = "";
+                    x4.Text = "";
+                    x5.Text = "";
+                    x6.Text = "";
+                    x7.Text = "";
+                    x8.Text = "";
+                    x9.Text = "";
+                    this.BackgroundImage = Properties.Resources.defaem;
+                }
+
+            }
+        }
+    }
+
+}
